cf1440a850654e863ae781a836035df3d9254b8a
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-02-19 21:48:47 -0500

    add about section

---
+++ src/app/about/page.js
@@ -0,0 +1,37 @@
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to my movie database website! I am a passionate movie enthusiast
+        who have come to create a one-stop destination for all your
+        movie-related needs.
+      </p>
+
+      <p>
+        My website is designed to provide you with a comprehensive database of
+        movies from all around the world, along with the latest news, reviews,
+        and trailers. My movie database is constantly updated with new releases,
+        ensuring that you have access to the latest and greatest in the world of
+        cinema. You can search for movies by title, director, actor, genre, or
+        release date, making it easy to find the perfect movie for any occasion.
+      </p>
+
+      <p>
+        In addition to my extensive movie database, I also offer a platform for
+        movie lovers to connect and share their thoughts on the latest releases.
+        Community section includes a forum where you can discuss your favorite
+        films with like-minded individuals and read reviews and ratings from
+        other users. It also has a section dedicated to movie news and trailers,
+        keeping you updated with the latest happenings in the world of cinema.
+        Thank you for visiting my website and I hope you enjoy your time
+        browsing through our movie database. If you have any feedback or
+        suggestions, please feel free to contact me. I am always looking for
+        ways to improve and enhance the user experience on the website. Happy
+        browsing!
+      </p>
+    </div>
+  );
+}
+
+export default About;
