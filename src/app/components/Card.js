8a48b32ad74da9d58ec2cfbed0bed89ad9eedc23
Oguzhan Demiraslan <oguzhandemiraslan@gmail.com>
2023-02-23 21:32:21 -0500

    update card component

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FiThumbsUp } from "react-icons/fi";
 
 function Card({ result }) {
   return (
-    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200">
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
       <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
@@ -11,8 +12,27 @@ function Card({ result }) {
           }`}
           width={500}
           height={300}
-          className="sm:rounded-t-lg"
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          alt="image is not available"
         ></Image>
+        <div className="p-2">
+          <h2 className="truncate text-lg font-bold mb-3">
+            {result.title || result.name}
+          </h2>
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+          <div className="flex justify-between mt-3">
+            <p>{result.release_date || result.first_air_date}</p>
+            <p className="flex  items-center">
+              <FiThumbsUp className="h-5 mr-1 ml-3" /> {result.vote_count}
+            </p>
+          </div>
+        </div>
       </Link>
     </div>
   );
